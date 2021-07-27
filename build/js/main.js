631f9848eb6dc0b7c1168071b48112646b0b9f2d
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2021-07-27 23:52:54 +0300

    Закончил вёрстку карточек каталога на главной

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,13 @@
+// Слайдер в первом экране главной страницы
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoHeight: true,
+  /* autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  }, */
+});
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIiJdfQ==
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLyDQodC70LDQudC00LXRgCDQsiDQv9C10YDQstC+0Lwg0Y3QutGA0LDQvdC1INCz0LvQsNCy0L3QvtC5INGB0YLRgNCw0L3QuNGG0YtcbnZhciBzd2lwZXIgPSBuZXcgU3dpcGVyKFwiLm15U3dpcGVyXCIsIHtcbiAgcGFnaW5hdGlvbjoge1xuICAgIGVsOiBcIi5zd2lwZXItcGFnaW5hdGlvblwiLFxuICB9LFxuICBhdXRvSGVpZ2h0OiB0cnVlLFxuICAvKiBhdXRvcGxheToge1xuICAgIGRlbGF5OiAyNTAwLFxuICAgIGRpc2FibGVPbkludGVyYWN0aW9uOiBmYWxzZSxcbiAgfSwgKi9cbn0pO1xuIl19
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// Слайдер в первом экране главной страницы
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoHeight: true,
+  /* autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  }, */
+});
